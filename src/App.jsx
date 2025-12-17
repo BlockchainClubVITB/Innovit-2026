45141e3ff725228367307d46b74fa5b7ed64575a
shahabhilash <abhilashshah2006@gmail.com>
2025-12-17 22:57:48 +0530

    chore(update): Update the App with all new features and fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Timeline from './components/Timeline';
 import Rounds from './components/Rounds';
@@ -6,6 +7,7 @@ import Prizes from './components/Prizes';
 import Mentoring from './components/Mentoring';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 import { GlobalSpotlight } from './components/MagicEffects';
 import { useIsMobile } from './hooks/useIsMobile';
 
@@ -13,24 +15,35 @@ function App() {
   const isMobile = useIsMobile();
 
   return (
-    <div className="min-h-screen">
-      {/* Navbar */}
-      <Navbar />
+    <BrowserRouter>
+      <div className="min-h-screen">
+        {/* Navbar */}
+        <Navbar />
 
-      {/* Disable GlobalSpotlight on mobile for performance */}
-      <GlobalSpotlight
-        enabled={!isMobile}
-        spotlightRadius={300}
-        glowColor="245, 188, 34"
-        targetSelector=".magic-card"
-      />
-      <Hero />
-      <Timeline />
-      <Rounds />
-      <Mentoring />
-      <Prizes />
-      <Footer />
-    </div>
+        {/* Disable GlobalSpotlight on mobile for performance */}
+        <GlobalSpotlight
+          enabled={!isMobile}
+          spotlightRadius={300}
+          glowColor="245, 188, 34"
+          targetSelector=".magic-card"
+        />
+
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Timeline />
+              <Rounds />
+              <Mentoring />
+              <Prizes />
+            </>
+          } />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
